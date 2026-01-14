7693d9db237c5218b79ae8e9dcc970ff349b5e5c
SG170 <swati.gouda2024@vitstudent.ac.in>
2026-01-14 23:42:17 +0530

    Add AppConstants class for application configuration
    Defines application constants including name, tagline, platform fee, and Chennai areas.

---
+++ lib/utils/constants.dart
@@ -0,0 +1,17 @@
+class AppConstants {
+  static const String appName = 'ReDrip';
+  static const String appTagline = 'Sustainable Fashion Marketplace';
+  static const double platformFee = 10.0;
+  static const List<String> chennaiAreas = [
+    'Adyar',
+    'Anna Nagar',
+    'Besant Nagar',
+    'Chrompet',
+    'Egmore',
+    'Kodambakkam',
+    'Mylapore',
+    'Nungambakkam',
+    'T Nagar',
+    'Velachery'
+  ];
+}
